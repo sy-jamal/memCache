70bb293328ac3842382623104be264752f5ead3a
Ubuntu <ubuntu@memcached.cloud.compas.cs.stonybrook.edu>
2019-04-11 13:09:26 +0000

    without memcache

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 const mysql = require('mysql');
+var Memcached = require('memcached');
+var memcached = new Memcached('192.168.122.30:11211');
 var app = express();
 const bodyParser= require('body-parser');
 
@@ -28,26 +30,63 @@ res.send('Hello World!')
 })
 
 app.get('/hw7', (req, res)=>{
-    console.log(req.query.club);
-    let playerQuery = 'SELECT * FROM `assists` WHERE Club = "'+ req.query.club+
+    let key = req.query.club+req.query.pos;
+    console.log(key);
+
+    // memcached.get(key,function(err) {
+    //     console.log("inside get")
+    //     if(err)
+    //     {
+            // console.log("err")        
+            let playerQuery = 'SELECT Club as club, POS as pos,A as max_assists, Player as player FROM `assists` WHERE Club = "'+ req.query.club+
                                                 '" AND POS = "'+ req.query.pos+ 
                                                 '" AND A = (SELECT MAX(A) from `assists` WHERE Club ="'+req.query.club+
-                                                                                        '" AND POS = "'+ req.query.pos+ '")';                                                                
+                                                                '" AND POS = "'+ req.query.pos+ '")ORDER BY GS DESC, Player'; 
+        
+            db.query(playerQuery, (err, docs)=>{
+                if(err){
+                    return res.status(500).send(err);
+                }
+                else{
+                    // console.log("inside query")
+                    let doc= docs[0];
+                    let avgQuery = 'SELECT AVG(A) as av FROM `assists` WHERE Club = "'+ req.query.club+'" AND POS = "'+ req.query.pos+ '"';
+                    db.query( avgQuery, (err, avg)=>{
+                        if(err)
+                            return res.status(500).send(err);
+                        else
+                        {   
+                            // console.log("inside avg")                 
+                            doc.avg_assists=avg[0].av;
+                            // memcached.set(key, doc, 10, function (err) 
+                            // {                     
+                                // console.log("inside set memcached") 
+                                console.log(doc.player);           
+                                return res.send(doc);
+                            // });                           
+                        }                
+                    })           
+                }
+            }); 
+        // }
+        // else{  
+        //     console.log("inside else")
+        //     memcached.touch('key', 10, function (err) {
+        //         console.log("inside touch")
+        //         return res.send(data);
+        //     });          
+               
+        // }
+    })
+                                                                   
+    // let returnQuery = 'SELECT Club as club, POS as pos,A as max_assists, Player as player, AVG(A) as avg_assists'+
+    //                                             ' FROM `assists` WHERE Club = "'+ 
+    //                                             req.query.club+'" AND POS = "'+ req.query.pos+ 
+    //                                             '" ORDER BY A DESC, GS DESC';                                                                
     
-    db.query(playerQuery, (err, docs)=>{
-        if(err){
-            return res.status(500).send(err);
-        }
-        else{
-            if(docs.lenght==1)
-                return res.json({status: "ok", players:docs[0]})
-            else{
-                let count = docs.lenght;
-                return res.json({status: "ok", players:docs})
-            }
-        }
-    });   
-});
+
+      
+// });
 
 
 app.listen(8080, '192.168.122.30');
